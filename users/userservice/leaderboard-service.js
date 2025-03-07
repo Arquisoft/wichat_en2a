0dc9206d828d07898e9551aecf5a2510fd5f5f6e
uo288764 <uo288764@uniovi.es>
2025-03-07 11:50:05 +0100

    Added leaderboard-service.js

---
+++ users/userservice/leaderboard-service.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Score = require('./score-model');
+const User = require('./user-model');
+
+
+//Claves relevantes nombre coleccion Usuarios! = 'users'
+// en bd llamar totalScore 
+
+const app = express();
+const port = 8004;
+
+// Middleware to parse JSON in request body
+app.use(express.json());
+
+// Connect to MongoDB
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/userdb';
+mongoose.connect(mongoUri);
+
+// endpoint for LEaderboard
+app.get('/leaderboard', async (req, res) => {
+    try {
+        const scores = await Score.aggregate([
+            {
+                $group: {
+                    _id: '$userId',
+                    totalScore: { $sum: '$score' },
+                    gamesPlayed: { $count: {} },
+                },
+            },
+            {
+                $lookup: {
+                    from: 'users', // Importante nombre coleccion Usuarios! = 'users'
+                    localField: '_id',
+                    foreignField: '_id',
+                    as: 'userInfo',
+                },
+            },
+            {
+                $unwind: '$userInfo',
+            },
+            {
+                $project: {
+                    username: '$userInfo.username',
+                    totalScore: 1,
+                    gamesPlayed: 1,
+                    winPercentage: {
+                        $multiply: [
+                            { $divide: ['$totalScore', { $add: ['$totalScore', 1] }] }, // totalScore + 1
+                            100,
+                        ],
+                    },
+                },
+            },
+            { $sort: { totalScore: -1 } }, // order in js by total score 
+            //  could be by games played, winrate, avg points... to be implemented
+        ]);
+
+        res.json(scores);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Start the server
+const server = app.listen(port, () => {
+    console.log(`Leaderboard Service listening at http://localhost:${port}`);
+});
+
+// Listen for the 'close' event on the Express.js server
+server.on('close', () => {
+    // Close the Mongoose connection
+    mongoose.connection.close();
+  });
+
+module.exports = server
