891bc73d2bd84b1c260c6d7371ea198a28418338
uo288764 <uo288764@uniovi.es>
2025-03-08 20:49:48 +0100

    Returned to default  user-service.js version

--- users/userservice/user-service.js
+++ users/userservice/user-service.js
@@ -1,8 +1,9 @@
+// user-service.js
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const { check, validationResult } = require('express-validator');
-const User = require('./user-model');
+const User = require('./user-model')
+
 const app = express();
 const port = 8001;
 
@@ -12,6 +13,8 @@ app.use(express.json());
 // Connect to MongoDB
 const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/userdb';
 mongoose.connect(mongoUri);
+
+
 
 // Function to validate required fields in the request body
 function validateRequiredFields(req, requiredFields) {
@@ -26,46 +29,20 @@ app.post('/adduser', async (req, res) => {
     try {
         // Check if required fields are present in the request body
         validateRequiredFields(req, ['username', 'password']);
+
         // Encrypt the password before saving it
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
         const newUser = new User({
             username: req.body.username,
             password: hashedPassword,
         });
+
         await newUser.save();
         res.json(newUser);
     } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
-
-app.post('/updateProfilePicture', [
-    // Validate inputs
-    check('username').isString().trim().escape(),
-    check('profilePicture').isURL().trim().escape()
-], async (req, res) => {
-    // Check for validation errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    try {
-        const { username, profilePicture } = req.body;
-
-        // Update user profile pic
-        const user = await User.findOneAndUpdate(
-            { username },
-            { profilePicture },
-            { new: true }
-        );
-        if (!user) {
-            return res.status(404).json({ error: 'User not found' });
-        }
-        res.json({ message: 'Profile picture updated successfully', user });
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
+        res.status(400).json({ error: error.message }); 
+    }});
 
 const server = app.listen(port, () => {
   console.log(`User Service listening at http://localhost:${port}`);
@@ -75,6 +52,6 @@ const server = app.listen(port, () => {
 server.on('close', () => {
     // Close the Mongoose connection
     mongoose.connection.close();
-});
+  });
 
-module.exports = server;
+module.exports = server
