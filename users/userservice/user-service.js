f50a8d498b9878559e32bdd28316415a4d2c6f30
uo288764 <uo288764@uniovi.es>
2025-03-08 20:41:33 +0100

    Solved problem related to express Validator

--- users/userservice/user-service.js
+++ users/userservice/user-service.js
@@ -1,10 +1,8 @@
-// user-service.js
 const express = require('express');
 const mongoose = require('mongoose');
-const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
-const User = require('./user-model')
-
+const { check, validationResult } = require('express-validator');
+const User = require('./user-model');
 const app = express();
 const port = 8001;
 
@@ -14,8 +12,6 @@ app.use(express.json());
 // Connect to MongoDB
 const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/userdb';
 mongoose.connect(mongoUri);
-
-
 
 // Function to validate required fields in the request body
 function validateRequiredFields(req, requiredFields) {
@@ -30,20 +26,18 @@ app.post('/adduser', async (req, res) => {
     try {
         // Check if required fields are present in the request body
         validateRequiredFields(req, ['username', 'password']);
-
         // Encrypt the password before saving it
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
         const newUser = new User({
             username: req.body.username,
             password: hashedPassword,
         });
-
         await newUser.save();
         res.json(newUser);
     } catch (error) {
-        res.status(400).json({ error: error.message }); 
-}});
+        res.status(400).json({ error: error.message });
+    }
+});
 
 app.post('/updateProfilePicture', [
     // Validate inputs
@@ -74,7 +68,7 @@ app.post('/updateProfilePicture', [
 });
 
 const server = app.listen(port, () => {
-    console.log(`User Service listening at http://localhost:${port}`);
+  console.log(`User Service listening at http://localhost:${port}`);
 });
 
 // Listen for the 'close' event on the Express.js server
