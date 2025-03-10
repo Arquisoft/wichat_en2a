12fbb316022d90bf3f6f70c77173aac2f58b5c38
PabloRP275 <pablorocesp@gmail.com>
2025-03-10 22:05:45 +0100

    Solve problem of test v.3

--- llmservice/llm-service.test.js
+++ llmservice/llm-service.test.js
@@ -12,7 +12,9 @@ jest.mock("axios");
 function generateTemplateMocks() {
   axios.post.mockImplementation((url, data) => {
     if (url.startsWith("Https://docs.g")) {
-      return Promise.resolve("llmanswer");
+      return Promise.resolve({
+        response: { text: () => Promise.resolve("llmanswer") },  // Simula que text() devuelve una promesa resuelta con "llmanswer"
+      });
     } else if (url.startsWith("https://empathyai")) {
       // response.data.choices[0]?.message?.content,
       return Promise.resolve({
