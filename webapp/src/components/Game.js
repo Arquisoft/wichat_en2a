96c2add643fc9d9e9d462e172266e6e6b863e4be
RaquelSuarezSanchez <uo295000@uniovi.es>
2025-03-31 23:16:48 +0200

    Trying to fix Game.js

--- webapp/src/components/Game.js
+++ webapp/src/components/Game.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import { Container, Typography, Button, Box, CircularProgress, Grid } from '@mui/material';
+import {Container, Typography, Button, Box, CircularProgress} from '@mui/material';
 import Navbar from './Navbar';
 import './game-styles.css';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import Timer from './Timer';
 
 const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
@@ -13,32 +13,50 @@ const Game = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [answerSelected, setAnswerSelected] = useState(false);
+
     const [correctAnswer, setCorrectAnswer] = useState(null);
     const [isCorrect, setIsCorrect] = useState(null);
     const [chosenAnswer, setChosenAnswer] = useState(null);
+
     const [timerKey, setTimerKey] = useState(0);
-    const [questionCount, setQuestionCount] = useState(0);
-    const [score, setScore] = useState(0);
-    const [hint, setHint] = useState(null);
-    const navigate = useNavigate();
 
     const MAX_QUESTIONS = 10;
+    const [questionCount, setQuestionCount] = useState(0);
+    const [score, setScore] = useState(0);
+
+    const [hint, setHint] = useState(null);
+
     const COLORS = {
         primary: '#6A5ACD',
         success: '#4CAF50',
         error: '#F44336',
-        hover: '#1565c0'
+        hover: '#1565c0',
+        textOnColor: 'white'
     };
 
+    const navigate = useNavigate();
+    // Fetch question from the API
     const fetchQuestion = async () => {
-        if (questionCount >= MAX_QUESTIONS) return endGame();
+        if (questionCount >= MAX_QUESTIONS) {
+            return endGame();
+        }
+
         try {
             setHint(null);
-            const response = await axios.get(`${apiEndpoint}/question`);
+            console.log("Fetching question...");
+            let response = await axios.get(`${apiEndpoint}/question`);
+            //if for some reason a problem occurred and the questions collection is empty, fetch
+            if (!response.data || response.data.length === 0) {
+                console.log("No questions found, initializing database...");
+                await axios.post(`${apiEndpoint}/fetch-flag-data`);
+                console.log("Database initialized. Fetching question again...");
+                response = await axios.get(`${apiEndpoint}/question`);
+            }
             setQuestion(response.data);
-            setTimerKey(prevKey => prevKey + 1);
-            setQuestionCount(prev => prev + 1);
+            setTimerKey((prevKey) => prevKey + 1); //to reload timer
+            setQuestionCount((prevCount) => prevCount + 1);
         } catch (error) {
+            console.error('Error fetching question:', error);
             setError('Failed to load question');
         } finally {
             setLoading(false);
@@ -67,10 +85,15 @@ const Game = () => {
 
     const retrieveHint = async () => {
         try {
-            const response = await axios.post(`${apiEndpoint}/askllm`, { question: question.correctAnswer, model: "empathy" });
+            const response = await axios.post(`${apiEndpoint}/askllm`, {
+                question: question.correctAnswer,
+                model: "empathy"
+            });
+            console.log('Hint received:', response.data.answer);
             setHint(response.data.answer);
         } catch (error) {
-            setError('Fetching hint failed');
+            setError(error.response?.data?.error || 'Fetching hint failed');
+            console.error('Error fetching hint:', error);
         }
     };
 
@@ -79,65 +102,200 @@ const Game = () => {
             setChosenAnswer(answer);
             const response = await axios.post(`${apiEndpoint}/check-answer`, {
                 questionId: question._id,
-                selectedAnswer: answer
+                selectedAnswer: answer,
             });
+
             setIsCorrect(response.data.isCorrect);
+
             if (response.data.isCorrect) setScore(prevScore => prevScore + 100);
-            if (!response.data.isCorrect) setCorrectAnswer(question.correctAnswer);
+
+            if (!response.data.isCorrect) {
+                setCorrectAnswer(question.correctAnswer);
+                // Aunque el usuario haya fallado, guardo la correcta
+            }
         } catch (error) {
+            console.error('Error checking answer:', error);
             setError('Failed to check answer');
         }
     };
 
-    useEffect(() => { fetchQuestion(); }, []);
+    // Función que se ejecuta cuando el tiempo se agota
+    const handleTimeUp = () => {
+        if (!answerSelected) {
+            setChosenAnswer(question.correctAnswer); // Marca la respuesta correcta
+            setIsCorrect(false); // No fue seleccionada por el usuario, así que es incorrecta
+            setCorrectAnswer(question.correctAnswer); // Muestra la respuesta correcta en verde
+            setAnswerSelected(true); // Evita más respuestas
+        }
+    };
 
-    if (loading) return <Container><CircularProgress /></Container>;
-    if (error) return <Container><Typography color='error'>{error}</Typography></Container>;
+    useEffect(() => {
+        fetchQuestion();
+    }, []);
+
+    if (loading) {
+        return (
+            <Container component="main" maxWidth="xl"
+                       sx={{textAlign: 'center', mt: '2rem', minHeight: '85vh', width: '100%', px: '1rem'}}>
+                <Typography component="h1" variant="h4" sx={{mb: '2rem'}}>
+                    Quiz Game!
+                </Typography>
+                <CircularProgress/>
+            </Container>
+        );
+    }
+
+    if (error) {
+        return (
+            <Container component="main" maxWidth="xl"
+                       sx={{textAlign: 'center', mt: '2rem', minHeight: '85vh', width: '100%', px: '1rem'}}>
+                <Typography component="h1" variant="h4" sx={{mb: '2rem', color: 'red'}}>
+                    {error}
+                </Typography>
+            </Container>
+        );
+    }
 
     return (
         <>
             <Navbar />
-            <Container>
-                <Typography variant="h4">Quiz Game!</Typography>
+            <Container component="main" maxWidth="xl"
+                       sx={{textAlign: 'center', mt: '0.5rem', minHeight: '85vh', width: '100%', px: '1rem'}}>
+                <Typography component="h1" variant="h4" sx={{mb: '1rem'}}>
+                    Quiz Game!
+                </Typography>
                 <Typography variant="h6">Score: {score} / {MAX_QUESTIONS * 100}</Typography>
-                <Typography>Question {questionCount} of {MAX_QUESTIONS}</Typography>
-                <Timer key={timerKey} duration={40} onTimeUp={() => setAnswerSelected(true)} answerSelected={answerSelected} />
-                <Grid container spacing={2} alignItems="center">
-                    <Grid item xs={12} md={6}>
-                        {question.options.map((option, index) => (
-                            <Button
-                                key={index}
-                                variant="contained"
-                                fullWidth
-                                sx={{ backgroundColor: answerSelected && option === correctAnswer ? COLORS.success : COLORS.primary, marginBottom: 1 }}
-                                disabled={answerSelected}
-                                onClick={() => { setAnswerSelected(true); checkAnswer(option); }}
-                            >
-                                {option}
-                            </Button>
-                        ))}
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <Typography variant="h6">Which country is this flag from?</Typography>
+
+                {/* Timer */}
+                <Timer key={timerKey} duration={40} onTimeUp={handleTimeUp} answerSelected={answerSelected} />
+
+                <Box sx={{
+                    display: 'flex',
+                    width: '100%',
+                    minHeight: '40vh',
+                    maxHeight: '60vh',
+                    gap: '1rem',
+                    flexDirection: 'row',
+                    overflow: 'auto'
+                }}>
+                    {/* Left side - 1/3 (antes era 2/3) */}
+                    <Box sx={{flex: 1, display: 'flex', flexDirection: 'column', gap: '1rem', width: '100%'}}>
+                        {/* Upper part - Question and answers */}
+                        <Box sx={{
+                            flex: 1,
+                            p: '1rem',
+                            border: '1px solid gray',
+                            borderRadius: '0.5rem',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            width: '100%'
+                        }}>
+                            <Typography variant="h6" sx={{mb: '1rem'}}>Which country is this flag from?</Typography>
+                            {question.options.map((option, index) => {
+                                let bgColor = COLORS.primary;
+
+                                // Solo cambiamos el color después de haber seleccionado una respuesta
+                                if (answerSelected) {
+                                    if (option === chosenAnswer) {
+                                        bgColor = isCorrect ? COLORS.success : COLORS.error;
+                                    } else if (option === correctAnswer) {
+                                        bgColor = COLORS.success;
+                                    }
+                                }
+
+                                return (
+                                    <Button
+                                        key={index}
+                                        variant="contained"
+                                        fullWidth
+                                        sx={{
+                                            mb: '0.5rem',
+                                            py: '1rem',
+                                            backgroundColor: bgColor,
+                                            "&:hover": {
+                                                backgroundColor: answerSelected ? undefined : COLORS.hover,
+                                            },
+                                            "&.Mui-disabled": {
+                                                backgroundColor: bgColor,
+                                                color: "white", // Asegura que el texto siga siendo visible si es necesario
+                                                opacity: 1, // Elimina la opacidad que Material-UI pone en los botones deshabilitados
+                                            }
+                                        }}
+                                        disabled={!!answerSelected}
+                                        onClick={() => {
+                                            if (!answerSelected) {
+                                                setAnswerSelected(true);
+                                                checkAnswer(option);
+                                            }
+                                        }}
+                                    >
+                                        {option}
+                                    </Button>
+                                );
+                            })}
+                        </Box>
+
+                        {/* Lower part - Hint box */}
+                        <Box sx={{
+                            flex: 1,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'space-between',
+                            p: '1.5rem',
+                            border: '1px solid gray',
+                            borderRadius: '0.5rem',
+                            width: '100%'
+                        }}>
+                            <Typography variant="h6" sx={{flex: 4}}>{hint || "Information hint here"}</Typography>
+                            <Button variant="contained" sx={{alignSelf: 'center', py: '1.5rem', fontSize: '1.5rem'}}
+                                    onClick={retrieveHint}>?</Button>
+                        </Box>
+                    </Box>
+
+                    {/* Right side - 2/3 (antes era 1/3) */}
+                    <Box sx={{
+                        flex: 2,
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        border: '1px solid gray',
+                        borderRadius: '0.5rem',
+                        width: '100%',
+                        height: 'auto',
+                        minHeight: '40vh',
+                        maxHeight: '60vh'
+                    }}>
                         {question?.imageUrl ? (
-                            <img 
-                                src={question.imageUrl} 
-                                alt="Flag" 
-                                style={{ 
-                                    width: '90%', 
-                                    maxHeight: '300px', 
-                                    objectFit: 'cover', 
-                                    borderRadius: 'inherit',
-                                    border: '1px solid black' 
-                                }} 
-                            />
+                            <img src={question.imageUrl} alt="Question related"
+                                 style={{width: '100%', height: '100%', objectFit: 'cover', borderRadius: 'inherit'}}/>
                         ) : (
                             <Typography variant="h6">No image available</Typography>
                         )}
-                    </Grid>
-                </Grid>
-                <Button variant="contained" onClick={retrieveHint}>Hint</Button>
-                <Button variant="contained" onClick={fetchQuestion} disabled={!answerSelected}>Next Question</Button>
+                    </Box>
+                </Box>
+
+                {/* Back and Next Question buttons */}
+                <Box sx={{mt: '0.5rem', display: 'flex', justifyContent: 'flex-start', width: '100%'}}>
+                    <Button variant="contained" color="error" onClick={() => navigate('/home')}>
+                        Back
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color = 'primary'
+                        disabled={!answerSelected} // Solo habilitado si se ha seleccionado una respuesta
+                        onClick={() => {
+                            setChosenAnswer(null);  // Reseteamos la respuesta elegida
+                            setCorrectAnswer(null); // Reseteamos la respuesta correcta
+                            setIsCorrect(null);     // Reseteamos el estado de corrección
+                            setAnswerSelected(false);
+                            setTimeout(() => fetchQuestion(), 100); // Cargamos nueva pregunta
+                        }}
+                        sx={{ ml: '1rem' }}
+                    >
+                        Next Question
+                    </Button>
+
+                </Box>
             </Container>
         </>
     );
